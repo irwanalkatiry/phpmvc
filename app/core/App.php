b8c8bc163210b181a9ae2afa893ef6d08f359a0f
ryugenxd <anonimous7770@gmail.com>
2023-09-11 20:46:17 +0800

    fixed bug di route di class  APP.php

--- app/core/App.php
+++ app/core/App.php
@@ -10,13 +10,16 @@ class App {
     
     
     $url = $this -> parseURL();
-    var_dump($this->parseURL());
     
-  
-    if(file_exists('../app/controllers/'. $url[0] . '.php')) {
-    $this->controller = $url[0];
-    
-   // unset($url[0]);
+    // jika variabel url index ke 0 tidak kosong
+    if (!empty($url[0])){
+
+      // cek apakah file controller ada
+      //  ucfist menguabah kata menjadi di awali huruf kapital
+      if(file_exists('../app/controllers/' . ucfirst($url[0]) . '.php')) {
+          $this->controller = ucfirst($url[0]);
+          unset($url[0]);
+      }
     }
     require_once '../app/controllers/' .$this->controller. '.php';
     //class controller diinstasiasi untuk memanggil method 
@@ -27,7 +30,6 @@ class App {
       if(method_exists($this->controller, $url[1])) {
         $this->method = $url[1];
         unset($url[1]);
-        
       }
     }
     
@@ -37,12 +39,14 @@ class App {
     //jalankan method dan controller serta masukkan paramater
     call_user_func_array([$this->controller, $this->method], $this->params);
   }
-  public function parseURL(){
-    if(isset ($_GET['url'])){
-    $url = rtrim($_GET['url'], '/');
-    $url = explode('/', $url);
-    return $url;
+  public function parseURL()
+  {
+    if(isset($_GET['url'])){
+      $url = rtrim($_GET['url'], '/');
+      $url = filter_var($url,FILTER_SANITIZE_URL);
+      $url = explode('/', $url);
+      return $url;
+    }
   }
-}
   
 }
--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 //teknik bootstraping (memanggil satu file mvc pada aplikasi )
 require_once'../app/init.php';
 
